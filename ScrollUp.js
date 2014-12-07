c74b2379b14f66ceb0c1ef53b8220f6d5d458edd
SoulTransfer <soultransfer@gmail.com>
2014-12-07 11:40:53 +0100

    1st Release

---
+++ ScrollUp.js
@@ -0,0 +1,24 @@
+﻿/**
+ * scrollup jQuery
+ * This script defines the functionality of the scrollup button which is located at the bottom of the page.
+ * It is required to use the scrollup.css file and the according img - furthermore please place the following tag on your page: <a href="#" class="scrollup">Scroll</a>
+ * @author Stefan Jud
+ */
+    $(document).ready(function () {
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('.scrollup').fadeIn();
+            } else {
+                $('.scrollup').fadeOut();
+            }
+        });
+
+        $('.scrollup').click(function () {
+            $("html, body").animate({
+                scrollTop: 0
+            }, 600);
+            return false;
+        });
+
+    })
---
+++ readme.md
@@ -0,0 +1,14 @@
+scrollUP
+========
+
+Just a very simply scrollUP jQuery Plugin.
+You simply assign it to any object, which is freely positionable, you want.
+#like: $("my_button").ScrollUpBox();#
+
+Options
+=======
+There are two options:
+$("my_button").ScrollUpBox({
+	rightPosition: '50px',
+	bottomPosition: '100px'
+});
